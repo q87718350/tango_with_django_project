fcfa33dffa59db0be28f3f46d7469e466ea80a1a
Li-Jen Chen <q87718350@gmail.com>
2021-05-31 15:27:40 +0100

    ch3 test done

--- Personal_Portfolio/Personal_Portfolio/urls.py
+++ Personal_Portfolio/Personal_Portfolio/urls.py
@@ -1,4 +1,4 @@
-"""tango_with_django_project URL Configuration
+"""Personal_Portfolio URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.2/topics/http/urls/
@@ -13,13 +13,10 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path, include
-from rango import views
+
+from django.urls import path
+from Personal_Portfolio.Personal_Portfolio import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', views.index, name='index'),
-    path('rango/', include('rango.urls')),
-    path('Personal_Portfolio/', include('Personal_Portfolio.Personal_Portfolio.urls'))
+    path('', views.homepage, name="Personal_Portfolio_home")
 ]
